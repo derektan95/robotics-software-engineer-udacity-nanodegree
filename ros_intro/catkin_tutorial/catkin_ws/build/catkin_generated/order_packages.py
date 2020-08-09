54f86c8a939d2f8bcf5ff782221890b3dd1bec6e
derektan95 <derektan95@hotmail.com>
2020-08-09 21:41:19 +0800

    Added tutorial for catkin workspace with sample 2 link robot arm.

---
+++ ros_intro/catkin_tutorial/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/derektan/Desktop/robotics_software_engineer_udacity/ros_intro/catkin_tutorial/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
