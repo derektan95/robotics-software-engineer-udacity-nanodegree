67aabe95ec089bdef59f88a216ccddeccf9a5d8a
derektan95 <derektan95@hotmail.com>
2020-08-14 21:31:02 +0800

    Initialized workspace for kalman filter implementation within gazebo turtlebot.

---
+++ kalman_filter/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/derektan/Desktop/robotics_software_engineer_udacity/kalman_filter/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
---
+++ kalman_filter/catkin_ws/build/gtest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/derektan/Desktop/robotics_software_engineer_udacity/kalman_filter/catkin_ws/build/gtest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
