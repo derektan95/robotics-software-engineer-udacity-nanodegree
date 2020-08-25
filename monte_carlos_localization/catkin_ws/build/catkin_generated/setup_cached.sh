b58a8eaecc01e4e1e11d3d3589417d30199504f2
derektan95 <derektan95@hotmail.com>
2020-08-26 01:15:33 +0800

    Completed project with final edits on acml.launch file.

--- monte_carlos_localization/catkin_ws/build/catkin_generated/setup_cached.sh
+++ monte_carlos_localization/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,4 +8,3 @@
 
 # modified environment variables
 export PWD='/home/derektan/Desktop/robotics_software_engineer_udacity/monte_carlos_localization/catkin_ws/build'
-export ROS_PACKAGE_PATH='/home/derektan/Desktop/robotics_software_engineer_udacity/monte_carlos_localization/catkin_ws/src:/opt/ros/melodic/share'
